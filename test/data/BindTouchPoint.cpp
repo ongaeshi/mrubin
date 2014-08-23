5c99df9bb8a987fd4dcba8ec5d530eab56854e50
ongaeshi <ongaeshi0621@gmail.com>
2014-08-23 22:29:50 +0900

    Add test/data/touch_point.mrubin

---
+++ test/data/BindTouchPoint.cpp
@@ -0,0 +1,15 @@
+#include "mruby.h"
+#include "mruby/value.h"
+
+namespace {
+
+}
+
+void color_init(mrb_state* mrb)
+{
+    struct RClass *cc = mrb_define_class(mrb, "Color", mrb->object_class);
+
+
+
+
+}
